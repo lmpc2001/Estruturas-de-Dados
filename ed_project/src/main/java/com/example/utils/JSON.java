4ae2b4e6d18447ab677be2f3bdb69c01b3387c51
Luís Costa <luis.pirescosta2@gmail.com>
2024-01-11 23:55:31 +0000

    Json class for all JSON related methods
    Signed-off-by: Luís Costa <luis.pirescosta2@gmail.com>

---
+++ ed_project/src/main/java/com/example/utils/JSON.java
@@ -0,0 +1,13 @@
+package com.example.utils;
+
+import com.example.domain.GameMap;
+
+public class JSON {
+	public void saveGameMap(GameMap map) {
+
+	}
+
+	public void uploadGameMap(GameMap map) {
+
+	}
+}
