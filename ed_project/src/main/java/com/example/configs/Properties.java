e4a1484e81ca4fda262ee981731b1992fc212178
Luís Costa <luis.pirescosta2@gmail.com>
2024-01-13 00:39:08 +0000

    Project Constants
    Signed-off-by: Luís Costa <luis.pirescosta2@gmail.com>

---
+++ ed_project/src/main/java/com/example/configs/Properties.java
@@ -0,0 +1,6 @@
+package com.example.configs;
+
+public final class Properties {
+	public static final String SAVE_GAMEMAP_FILE_PATH = "";
+	public static final String UPLOAD_GAMEMAP_FILE_PATH = "";
+}
